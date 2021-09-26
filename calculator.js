3b3604489071a48b1651caf9f2e5856e5900c9ea
Chris Cresswell <christorper@gmail.com>
2021-09-25 20:46:17 -0700

    Add display logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
+let display = document.querySelector('p');
+
+let buttons = document.querySelectorAll('.display');
+buttons = Array.from(buttons);
+
+let input = [];
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    if(input.length > 10)
+    {
+        return 0;
+    }
+    input.push(button.textContent);
+    display.textContent += button.textContent;
+}))
+
 function add(x, y) {
     return x + y;
 }
@@ -14,6 +30,27 @@ function divide(x, y) {
     return x / y;
 }
 
-function operate (x, y, operator) {
-    return operator(x, y);
+function operate (x, operator, y) {
+    switch(operator) {
+
+        case '+':
+            return add(x, y);
+            break;
+
+        case '-':
+            return subtract(x, y);
+            break;
+
+        case '×':
+            return multiply(x, y);
+            break;
+
+        case '÷':
+            return divide(x, y);
+            break;
+
+        default: 
+            return `${x} ${y}`;
+            break;
+    }
 }
